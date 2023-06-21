214b194a51f426b5ebaca281516c57d7763db1d2
davedev <davidabolade29@gmail.com>
2023-06-21 18:36:56 +0100

    Banner commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import star from "../assets/Star.png"
+import Marquee from "react-marquee-slider"
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="w-full overflow-hidden bg-black text-white font-aeon font-light text-[20px] leading-[24px] py-[30px] tracking-wider">
+      <Marquee speed={30} direction="left" gradient={false} className="flex justify-between">
+        <p className="mx-[64px]">Photochemical Analysis</p>
+      <img src={star} alt="star" className="w-[19px] h-[19px] mx-[64px]"/>
+      <p className="mx-[64px]">Pollutant Analysis</p>
+            <img src={star} alt="star" className="w-[19px] h-[19px] mx-[64px]"/>
+      <p className="mx-[64px]">Nutritional Analysis</p>
+            <img src={star} alt="star" className="w-[19px] h-[19px] mx-[64px]"/>
+      </Marquee>
+    </div>
   )
 }
 
