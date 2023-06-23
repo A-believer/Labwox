419b8b5d8005955591654142e0a51d5aedda6a36
davedev <davidabolade29@gmail.com>
2023-06-23 05:56:02 +0100

    cta mobile responsible

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,7 +3,7 @@ import Button from "../utils/Button"
 const Cta = () => {
   return (
     <div className="bg-ctaBg bg-contain bg-center lg:px-[160px] px-[16px] lg:py-[167px] pt-[40px] pb-[56px] flex flex-col justify-center items-center">
-      <div className="text-center lg:text-[52px] lg:leading-[62.4px] lg:pb-[32px] font-semibold">
+      <div className="text-center lg:text-[52px] text-[24px] lg:leading-[62.4px] leading-[28.8px] pb-[32px] font-semibold">
         <p>Find out how partnering with</p>
         <p>Labworks can help you achieve</p>
         <p>tangible research and learning capabilities.</p>
