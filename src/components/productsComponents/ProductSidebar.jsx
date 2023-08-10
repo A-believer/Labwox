c51830b827346332271a7c180bad1cfac182f4fd
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-08-10 04:25:50 +0100

    product page touch up

--- src/components/productsComponents/ProductSidebar.jsx
+++ src/components/productsComponents/ProductSidebar.jsx
@@ -6,14 +6,13 @@ const ProductSidebar = () => {
     const [view, setView] = useState(false)
     
   return (
-      <section className="bg-white lg:pt-7 pt-0 lg:px-[14px] px-0 lg:pb-[600px] pb-5 text-blackii text-base leading-5 font-normal rounded">
+      <section className="lg:bg-white bg-none lg:pt-7 pt-0 lg:px-[14px] px-0 lg:pb-[600px] pb-0 text-blackii text-base leading-5 font-normal rounded shadow-2xl">
           
           {/* Desktop View  */}
           <div className="lg:block hidden">
-               <p>Search Test</p> 
           <div className="mt-2 flex justify-center items-center">
-              <input type="text"  className="rounded-l w-[282px] h-[47px] border border-grey ring-0 outline-none pl-4"/>
-              <button type="button" className="bg-blackii text-white py-[14px] pl-6 pr-8 rounded-r">Search</button>
+              <input placeholder="enter test..." type="text"  className="rounded-l w-[282px] h-[47px] border border-orange ring-0 outline-none pl-4"/>
+              <button type="button" className="bg-orange text-white py-[14px] pl-6 pr-8 rounded-r">Search</button>
          </div> 
           <div className="mt-8 bg-white">
               <p className="font-bold">Categories</p>
@@ -24,11 +23,11 @@ const ProductSidebar = () => {
           
           {/* Mobile View  */}
 
-          <div className="lg:hidden block">
+          <div className="lg:hidden block bg-clear">
               
-              <div className="mt-2 flex justify-center items-center w-full">
-                  <input type="text" className="rounded-l h-[47px] w-[70%] border border-grey ring-0 outline-none pl-4"/>
-                  <button type="button" className="bg-blackii flex justify-center items-center  gap-2 text-white py-[12px] px-1 rounded-r w-[30%]" onClick={() => setView((prev) => !prev)}>
+              <div className=" flex justify-center items-center w-full">
+                  <input placeholder="enter test..." type="text" className="rounded-l h-[47px] w-[70%] border border-orange ring-0 outline-none pl-4"/>
+                  <button type="button" className="bg-orange flex justify-center items-center  gap-2 text-white py-[12px] px-1 rounded-r w-[30%]" onClick={() => setView((prev) => !prev)}>
                   <img src={filter} alt="filter" className="w-6 h-6" /> 
                   <span>Filter</span></button>
               </div>
--- src/components/productsComponents/Products.jsx
+++ src/components/productsComponents/Products.jsx
@@ -3,24 +3,24 @@ import { Link} from "react-router-dom"
 const Products = ({tests}) => {
   console.log(tests)
   return (
-    <section className="bg-white lg:px-5 px-2 lg:py-5 py-2 lg:my-5 my-2">
+    <section className="bg-white lg:px-5 px-4 lg:py-5 py-4 lg:my-0 my-2 rounded shadow-2xl">
       {tests.map((test) => (
-        <div key={test.id} className="bg-whitebgii lg:p-5 p-2 lg:mb-10 mb-5">
+        <div key={test.id} className="bg-whitebgii lg:p-5 p-2 lg:mb-10 mb-5 border border-orange shadow-2xl">
           <div className="flex justify-between items-center lg:gap-x-0 gap-x-4">
-            <div className="lg:text-xl text-base font-bold leading-6 text-blackii">
-              <p className="font-extrabold">{test.testTitle}</p>
+            <div className="lg:text-xl text-lg font-bold leading-6 text-blackii">
+              <p className="font-extrabold underline decoration-grey underline-offset-2">{test.testTitle}</p>
               <p className="my-1 ">{test.code}</p>
-              <p className="text-black">₦{ test.pricing}</p>
+              <p className="text-orange lg:my-5 my-2">₦{test.pricing}</p>
             </div>
 
-            <button className="self-start bg-white lg:py-[10px] py-1 lg:px-3 px-1 lg:text-lg text-xs font-medium lg:leading-6 leading-2 text-grey rounded border border-grey">
+            <button className="self-start bg-orange lg:py-[10px] py-1 lg:px-3 px-1 lg:text-lg text-xs font-medium lg:leading-6 leading-2 text-white rounded border border-orange">
               <Link to={test.id}>View Details</Link>
             </button>
           </div>
-          <div className="mt-4 lg:text-base text-sm leading-6 text-blackii font-medium">
-            <p>Category: <span className="font-normal">{ test.category}</span></p>
+          <div className="lg:text-xl text-lg leading-6 text-blackii font-medium">
+            <p>Category: <span className="text-grey font-normal lg:text-lg text-base">{ test.category}</span></p>
             <p>Parameters: </p>
-            <p className="text-grey font-normal">{test.parameters}</p>
+            <p className="text-grey font-normal lg:text-lg text-base">{test.parameters}</p>
           </div>  
         </div>
         
