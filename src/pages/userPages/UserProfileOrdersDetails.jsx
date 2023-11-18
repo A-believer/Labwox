17dd71fa1aee2b09b225fcee529a19a275284bc8
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-11-18 14:06:53 +0100

    update

--- src/pages/userPages/UserProfileOrdersDetails.jsx
+++ src/pages/userPages/UserProfileOrdersDetails.jsx
@@ -43,7 +43,7 @@ function UserProfileOrdersDetails() {
     async function payWithPaystack() {
      const paystack = new PaystackPop()
         await paystack.newTransaction({
-          key: import.meta.env.VITE_PAYSTACK_LIVE_KEY,
+          key: "pk_live_7d1a10bd189ae156ac3c3f360f851600f70b439e",
           amount: amount * 100,
           email,
           firstName,
