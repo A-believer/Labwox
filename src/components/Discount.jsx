0c51e747ab1b92a95f9b3497d3f8929eda00ee34
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-11-26 13:02:34 +0100

    discount update

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -6,7 +6,7 @@ function Discount({discountRef}) {
 
   return (
       <div ref={discountRef} className='absolute -bottom-28 right-10 text-xs p-4 bg-white shadow-2xl border border-orange'>
-          <p className='flex justify-between gap-x-5 font-bold'><span>Cart Items Number</span> <span>Discount Account Applied</span></p>
+          <p className='flex justify-between gap-x-5 font-bold'><span>Number of tests</span> <span>Discount</span></p>
           <p className='flex justify-between'><span>`&gt;`10</span><span>5%</span></p>
           <p className='flex justify-between'><span>`&gt;`20</span><span>8%</span></p>
           <p className='flex justify-between'><span>`&gt;`30</span><span>10%</span></p>
